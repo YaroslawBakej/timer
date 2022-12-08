d8949b5a88d31a7a06b428bce9ddfc235939e556
unknown <jarobak1995@gmail.com>
2022-12-08 12:15:07 +0300

    test

---
+++ app/app.js
@@ -0,0 +1,16 @@
+const hoursEl = document.querySelector('.hours'),
+    minutesEl = document.querySelector('.minutes'),
+    secondsEl = document.querySelector('.seconds'),
+    millisecondsEl = document.querySelector('.milliseconds')
+
+const start = document.querySelector('.start'),
+    stop = document.querySelector('.stop'),
+    pause = document.querySelector('.pause')
+
+let hours = 00,
+    minutes = 00,
+    seconds = 00,
+    milliseconds = 00,
+    interval
+
+
